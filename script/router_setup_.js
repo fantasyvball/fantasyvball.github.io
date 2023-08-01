5f2f744784ffe0b1db1502bccffdf68cc70eb76e
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2023-08-01 18:16:58 -0400

    Update router_setup_.js

--- script/router_setup_.js
+++ script/router_setup_.js
@@ -1,18 +1,18 @@
-import NavbarComponent from 'https://fantasyvball.github.io/components/NavbarComponent.js';
-import FooterComponent from 'https://fantasyvball.github.io/components/FooterComponent.js';
+import NavbarComponent from './components/NavbarComponent.js';
+import FooterComponent from './components/FooterComponent.js';
 
-import LoginComponent from 'https://fantasyvball.github.io/components/LoginComponent.js';
-import LogoutComponent from 'https://fantasyvball.github.io/components/LogoutComponent.js';
-import HomeComponent from 'https://fantasyvball.github.io/components/HomeComponent.js';
-import PlayerRosterComponent from 'https://fantasyvball.github.io/components/PlayerRosterComponent.js'
-import UserComponent from 'https://fantasyvball.github.io/components/UserComponent.js'
+import LoginComponent from './components/LoginComponent.js';
+import LogoutComponent from './components/LogoutComponent.js';
+import HomeComponent from './components/HomeComponent.js';
+import PlayerRosterComponent from './components/PlayerRosterComponent.js'
+import UserComponent from './components/UserComponent.js'
 
-import AvailablePlayersComponent from 'https://fantasyvball.github.io/components/AvailablePlayersComponent.js'
-import ArticleComponent from 'https://fantasyvball.github.io/components/ArticleComponent.js'
-import LeaderBoardComponent from 'https://fantasyvball.github.io/components/LeaderBoardComponent.js'
-import PlayerAchievmentComponent from 'https://fantasyvball.github.io/components/PlayerAchievementComponent.js'
-import NotFoundComponent from 'https://fantasyvball.github.io/components/NotFoundComponent.js'
-import StatsComponent from 'https://fantasyvball.github.io/components/StatsComponent.js'
+import AvailablePlayersComponent from './components/AvailablePlayersComponent.js'
+import ArticleComponent from './components/ArticleComponent.js'
+import LeaderBoardComponent from './components/LeaderBoardComponent.js'
+import PlayerAchievmentComponent from './components/PlayerAchievementComponent.js'
+import NotFoundComponent from './components/NotFoundComponent.js'
+import StatsComponent from './components/StatsComponent.js'
 
 const routes = [
   {path: "/", component: HomeComponent},
