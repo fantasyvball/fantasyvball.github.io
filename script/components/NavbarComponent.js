bf6d5bdd4c4f0a363dabe566ef42becf728ece56
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2023-08-03 18:41:06 -0400

    Update NavbarComponent.js

--- script/components/NavbarComponent.js
+++ script/components/NavbarComponent.js
@@ -52,11 +52,11 @@ export default{
     
     
     <!-- Logout Confirmation Modal -->
-    <div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby=logoutModalLabel" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content" style="background-color: #333; color: #fff; border-radius: 0.5rem;">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Logout Confirmation</h5>
+            <h5 class="modal-title" id="logoutModalLabel">Logout Confirmation</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
