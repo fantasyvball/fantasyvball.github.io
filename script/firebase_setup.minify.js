f8fa78ac81f6d85d77d69abdef8ed15bc1c0d385
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2023-08-04 16:33:05 -0400

    Update firebase_setup.minify.js

--- script/firebase_setup.minify.js
+++ script/firebase_setup.minify.js
@@ -1,1 +1,406 @@
-import{initializeApp}from"./firebase/firebase-app.js";import{getDatabase,ref,set,child,get}from"./firebase/db.js";import{initializeAppCheck,ReCaptchaV3Provider}from"./firebase/appcheck.js";import{getAuth,RecaptchaVerifier,signInWithPhoneNumber,updateProfile,isSignInWithEmailLink,sendSignInLinkToEmail,signInWithEmailLink}from"./firebase/auth.js";import my_super_list from"./player.js";import all_school from"./all_school.js";import conf_member from"./conf_member.js";const firebaseConfig={apiKey:"AIzaSyDrTUss_WCj1OS8h0xjIRj9aNWgtKMk_YI",authDomain:"fantasy-volleyball-88bff.firebaseapp.com",databaseURL:"https://fantasy-volleyball-88bff-default-rtdb.firebaseio.com",projectId:"fantasy-volleyball-88bff",storageBucket:"fantasy-volleyball-88bff.appspot.com",messagingSenderId:"370961046238",appId:"1:370961046238:web:5764c52184e7df03bbc04f",measurementId:"G-10JNHBDR2M"},fire_app=initializeApp(firebaseConfig),appCheck=initializeAppCheck(fire_app,{provider:new ReCaptchaV3Provider("6LfgoT8nAAAAACoykwmQxMLRTTPhBpBXOtYsKcdJ"),isTokenAutoRefreshEnabled:!0}),auth=getAuth();auth.languageCode="en";let my_secret_list=[];function handlePhoneNumberAuth(e){return new Promise(((t,r)=>{window.recaptchaVerifier=new RecaptchaVerifier(auth,"recaptcha-container",{});const s=window.recaptchaVerifier;signInWithPhoneNumber(auth,e,s).then((e=>{t(e)})).catch((e=>{r(e)}))}))}function handlePhoneNumberLog(e,t){return new Promise(((r,s)=>{e.confirm(t).then((e=>{r(e.user)})).catch((e=>{s(e)}))}))}function handleEmailAuth(e){return new Promise(((t,r)=>{sendSignInLinkToEmail(auth,e,{url:"https://untimelyslushyregister.volleyvisual.repl.co#?test=true",handleCodeInApp:!0}).then((()=>{t("true")})).catch((e=>{r(e)}))}))}function handleEmailLog(e,t){return new Promise(((r,s)=>{signInWithEmailLink(auth,e,t).then((e=>{r(e.user)})).catch((e=>{console.log(e),s(e)}))}))}function hasFireUser(){return auth.currentUser}function newUser(){return auth.currentUser&&null==auth.currentUser.displayName}function UpdateSubsribeStatus(t){updateProfile(auth.currentUser,{displayName:t}).then((()=>{console.log("success")})).catch((t=>{console.log(e)}))}function fireAuthOut(){null!=auth.currentUser&&auth.signOut().then((()=>{console.log("User logged out successfully.")})).catch((e=>{console.error("Error logging out: ",e)}))}function initList(){return new Promise(((e,t)=>{if(null==auth.currentUser){const t=localStorage.getItem("roster");if(null!=t){my_secret_list=t.split("\n");let e=my_secret_list.indexOf("");for(;-1!==e;)my_secret_list[e]=null,e=my_secret_list.indexOf("")}e()}else{const r=auth.currentUser.uid,s=ref(getDatabase());get(child(s,"users/"+r)).then((t=>{if(t.exists()){const e=t.val();for(let t=0;t<20;t++){const r=e[t];r&&r.includes("******************")&&t>7?my_secret_list[t]="null":r&&r.includes("******************")?my_secret_list[t]=null:r&&(my_secret_list[t]=r)}let r=my_secret_list.indexOf("null");for(;-1!=r;)my_secret_list.splice(r,1),r=my_secret_list.indexOf("null")}e()})).catch((e=>{console.log(e),t(e)}))}}))}function getUserRoster(e){return new Promise(((t,r)=>{const s=ref(getDatabase());get(child(s,"users/"+e)).then((e=>{let r=[];if(e.exists()){const t=e.val();for(let e=0;e<20;e++){const s=t[e];s&&s.includes("******************")&&e>7?r[e]="null":s&&s.includes("******************")?r[e]=null:s&&(r[e]=s)}let s=r.indexOf("null");for(;-1!=s;)r.splice(s,1),s=r.indexOf("null")}t(r)})).catch((e=>{console.log(e),r(e)}))}))}function secret_list_getter(){return my_secret_list.slice()}function secret_list_adder(e){if(my_secret_list.filter((e=>null!=e)).length>=10)return!1;if(20!=(e=String(e)).length)return!1;if(my_secret_list.length>=7)my_secret_list.push(e);else{my_secret_list[7]=e;for(let e=0;e<7;e++)my_secret_list[e]||(my_secret_list[e]=null)}return commitToLS(),commitToUser(),!0}function secrect_list_dropper(e){const t=my_secret_list.indexOf(e);return t>=7&&(my_secret_list.splice(t,1),commitToLS(),commitToUser(),!0)}function secret_list_sendBack(e){const t=my_secret_list.indexOf(e);return t<=6&&(my_secret_list[t]=null,my_secret_list.push(e),commitToLS(),commitToUser(),!0)}function secret_list_start(e,t){const r=my_secret_list.indexOf(t);return e<=6&&r>=0&&null==my_secret_list[e]&&(my_secret_list[e]=t,my_secret_list.splice(r,1),commitToLS(),commitToUser(),!0)}function commitToLS(){null==auth.currentUser&&localStorage.setItem("roster",my_secret_list.join("\n"))}function commitToUser(){if(null==auth.currentUser)return;const e=auth.currentUser.uid,t="users/"+e,r=getDatabase();let s=my_secret_list.slice();for(let e=0;e<11;e++)s[e]||(s[e]=null);let i=s.indexOf(null);for(;-1!==i;)s[i]=i<10?"******************0"+i:"******************"+i,i=s.indexOf(null);console.log({...s}),set(ref(r,t),{...s}).then((()=>{console.log("Data successfully committed to /users/"+e)})).catch((e=>{console.error("Error committing data: ",e)}))}function getAllPlayers(){return new Promise(((e,t)=>{0!=my_super_list.length?e(my_super_list.slice()):t()}))}function getPlayerById(e){if(null==e)return{};return my_super_list.find((t=>t.id===e))||{}}window.handlePhoneNumberAuth=handlePhoneNumberAuth,window.handlePhoneNumberLog=handlePhoneNumberLog,window.handleEmailAuth=handleEmailAuth,window.handleEmailLog=handleEmailLog,window.hasFireUser=hasFireUser,window.newUser=newUser,window.UpdateSubsribeStatus=UpdateSubsribeStatus,window.getAllPlayers=getAllPlayers,window.initList=initList,window.getUserRoster=getUserRoster,window.secret_list_getter=secret_list_getter,window.secret_list_adder=secret_list_adder,window.secrect_list_dropper=secrect_list_dropper,window.secret_list_sendBack=secret_list_sendBack,window.secret_list_start=secret_list_start,window.fireAuthOut=fireAuthOut,window.getPlayerById=getPlayerById,window.all_school=all_school,window.conf_member=conf_member;
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "./firebase/firebase-app.js";
+
+import { getDatabase, ref, set, child, get  } from "./firebase/db.js"
+
+import { initializeAppCheck, ReCaptchaV3Provider } from "./firebase/appcheck.js";
+
+import { getAuth, RecaptchaVerifier, signInWithPhoneNumber, updateProfile, isSignInWithEmailLink, sendSignInLinkToEmail, signInWithEmailLink } from "./firebase/auth.js"
+
+import my_super_list from "./player.js"
+
+import all_school from "./all_school.js"
+
+import conf_member from "./conf_member.js"
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDrTUss_WCj1OS8h0xjIRj9aNWgtKMk_YI",
+
+  authDomain: "fantasy-volleyball-88bff.firebaseapp.com",
+
+  databaseURL: "https://fantasy-volleyball-88bff-default-rtdb.firebaseio.com",
+
+  projectId: "fantasy-volleyball-88bff",
+
+  storageBucket: "fantasy-volleyball-88bff.appspot.com",
+
+  messagingSenderId: "370961046238",
+
+  appId: "1:370961046238:web:5764c52184e7df03bbc04f",
+
+  measurementId: "G-10JNHBDR2M"
+
+};
+
+
+// Initialize Firebase
+
+const fire_app = initializeApp(firebaseConfig);
+
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+const appCheck = initializeAppCheck(fire_app, {
+  provider: new ReCaptchaV3Provider('6LfgoT8nAAAAACoykwmQxMLRTTPhBpBXOtYsKcdJ'),
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: true
+});
+
+
+const auth = getAuth();
+auth.languageCode = 'en';
+
+
+let my_secret_list = []
+
+function handlePhoneNumberAuth(phoneNumber) {
+  return new Promise((resolve, reject) => {
+    window.recaptchaVerifier = new RecaptchaVerifier(auth, 'recaptcha-container', {});
+    const appVerifier = window.recaptchaVerifier;
+    signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+      .then((confirmationResult) => {
+        resolve(confirmationResult)
+      }).catch((error) => {
+        // Couldn't send code.
+        reject(error)
+      });
+
+  });
+}
+
+function handlePhoneNumberLog(confirmationResult, code) {
+  return new Promise((resolve, reject) => {
+    confirmationResult.confirm(code).then((result) => {
+      // User signed in successfully.
+      resolve(result.user)
+    }).catch((error) => {
+      // User couldn't sign in (bad verification code?).
+      reject(error)
+    });
+  });
+}
+
+function handleEmailAuth(email) {
+  return new Promise((resolve, reject) => {
+    sendSignInLinkToEmail(auth, email, {
+      // this is the URL that we will redirect back to after clicking on the link in mailbox
+      url: 'https://untimelyslushyregister.volleyvisual.repl.co#?test=true',
+      handleCodeInApp: true,
+    }).then(() => {
+      resolve("true")
+    }).catch((error) => {
+      reject(error)
+    });
+  });
+}
+
+function handleEmailLog(email, link) {
+  return new Promise((resolve, reject) => {
+    signInWithEmailLink(auth, email, link)
+      .then((result) => {
+        resolve(result.user)
+      }).catch((error) => {
+        console.log(error)
+        reject(error)
+      });
+  });
+}
+
+function hasFireUser() {
+  return auth.currentUser
+}
+
+function newUser() {
+  return auth.currentUser && auth.currentUser.displayName == null
+}
+
+function UpdateSubsribeStatus(val) {
+  updateProfile(auth.currentUser, {
+    displayName: val,
+  }).then(() => {
+    console.log("success")
+  }).catch((error) => {
+    console.log(e)
+  });
+
+}
+
+function fireAuthOut(){
+  if(auth.currentUser == null){
+    return
+  }
+  auth.signOut()
+  .then(() => {
+    // Log out successful
+    console.log("User logged out successfully.");
+  })
+  .catch((error) => {
+    // An error occurred while logging out
+    console.error("Error logging out: ", error);
+  });
+}
+
+// db starts here
+function initList() {
+  return new Promise((resolve, reject) => {
+    if (auth.currentUser == null) {
+      // TODO:load from local storage
+      const roster = localStorage.getItem('roster');
+      if(roster != null){
+        my_secret_list = roster.split("\n")
+        let index = my_secret_list.indexOf("");
+
+        while (index !== -1) {
+          my_secret_list[index] = null
+          index = my_secret_list.indexOf("");
+        }
+      }
+      else{
+        //pass
+      }
+    
+      resolve();
+    } else {
+      const uid = auth.currentUser.uid;
+      
+      const dbRef = ref(getDatabase());
+      get(child(dbRef, "users/"+uid)).then((snapshot) => {
+        if (snapshot.exists()) {
+          const snap_val = snapshot.val()
+          for(let i = 0; i < 20; i ++){
+            const j = snap_val[i]
+            if(j && j.includes("******************") && i>7){
+              my_secret_list[i] = "null"
+            }else if(j && j.includes("******************")){
+              my_secret_list[i] = null
+            }else if(j){
+              my_secret_list[i] = j
+            }
+          }
+          let i = my_secret_list.indexOf("null")
+          while(i != -1){
+            my_secret_list.splice(i,1)
+            i = my_secret_list.indexOf("null")
+          }
+        } else {
+          // ignore
+        }
+        resolve();
+      }).catch((error) => {
+        console.log(error);
+        reject(error);
+      });
+    } 
+  });
+}
+
+function getUserRoster(uid){
+  return new Promise((resolve, reject) => {
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, "users/"+uid)).then((snapshot) => {
+      let out = []
+      if (snapshot.exists()) {
+        const snap_val = snapshot.val()
+        for(let i = 0; i < 20; i ++){
+          const j = snap_val[i]
+          if(j && j.includes("******************") && i>7){
+            out[i] = "null"
+          }else if(j && j.includes("******************")){
+            out[i] = null
+          }else if(j){
+            out[i] = j
+          }
+        }
+        let i = out.indexOf("null")
+        while(i != -1){
+          out.splice(i,1)
+          i = out.indexOf("null")
+        }
+      } else {
+        // ignore
+      }
+      resolve(out);
+    }).catch((error) => {
+      console.log(error);
+      reject(error);
+    });
+    
+  });
+}
+  
+function secret_list_getter(){
+  return my_secret_list.slice()
+}
+
+function secret_list_adder(playerId){
+  // check if there's space availabe
+  if(my_secret_list.filter((obj) => {return obj != null}).length >= 10){
+    return false
+  }
+  playerId = String(playerId)
+  if(playerId.length != 20){
+    return false
+  }
+  if(my_secret_list.length >=7){
+    my_secret_list.push(playerId)
+  }else{
+    my_secret_list[7] = playerId
+    for(let i = 0;i<7;i++){
+      if(!my_secret_list[i]){
+        my_secret_list[i] = null
+      }
+    }
+  }
+  commitToLS()
+  commitToUser()
+  return true
+}
+
+function secrect_list_dropper(playerId){
+  const index = my_secret_list.indexOf(playerId)
+  if(index >= 7){
+    my_secret_list.splice(index,1)
+    commitToLS()
+    commitToUser()
+    return true
+  }
+  else{
+    return false
+  }
+}
+
+function secret_list_sendBack(playerId){
+  const index = my_secret_list.indexOf(playerId)
+  if(index <= 6){
+    my_secret_list[index] = null
+    my_secret_list.push(playerId)
+    commitToLS()
+    commitToUser()
+    return true
+  }else{
+    return false
+  }
+}
+
+function secret_list_start(index,playerId){
+  const pindex = my_secret_list.indexOf(playerId)
+  if(index <= 6 && pindex >=0 && my_secret_list[index] == null){
+    my_secret_list[index] = playerId
+    my_secret_list.splice(pindex,1)
+    commitToLS()
+    commitToUser()
+    return true
+  }
+  else{
+    return false
+  }
+}
+
+function commitToLS() {
+  if(auth.currentUser != null){
+    return
+  }
+  localStorage.setItem("roster", my_secret_list.join("\n"))
+}
+
+function commitToUser() {
+  if(auth.currentUser == null){
+    return
+  }
+  const uid = auth.currentUser.uid
+  const route = 'users/' + uid
+  const db = getDatabase();
+  // TODO:VALIDATE data  before commit
+  // https://firebase.google.com/docs/reference/security/database#newdata
+
+  let data = my_secret_list.slice()
+  
+  for(let i = 0;i<11;i++){
+    if(!data[i]){
+      data[i] = null
+    }
+  }
+
+  let index = data.indexOf(null);
+
+  while (index !== -1) {
+    if(index < 10){
+      data[index] = "******************0" + index
+    }
+    else{
+      data[index] = "******************" + index
+    }
+    index = data.indexOf(null);
+
+  }
+  console.log({...data})
+  set(ref(db, route), {...data})
+  .then(() => {
+    console.log("Data successfully committed to /users/" + uid);
+  })
+  .catch((error) => {
+    console.error("Error committing data: ", error);
+  });
+}
+
+// firestore starts here
+function getAllPlayers() {
+  return new Promise((resolve, reject) => {
+    if(my_super_list.length != 0){
+      resolve(my_super_list.slice())
+    }
+    else{
+      reject()
+    }
+  });
+}
+
+function getPlayerById(id) {
+  if(id == null){
+    return {}
+  }
+  let result = my_super_list.find(obj => {
+    return obj.id === id
+  })
+  return result || {}
+}
+
+
+
+
+
+window.handlePhoneNumberAuth = handlePhoneNumberAuth;
+window.handlePhoneNumberLog = handlePhoneNumberLog;
+window.handleEmailAuth = handleEmailAuth;
+window.handleEmailLog = handleEmailLog;
+
+window.hasFireUser = hasFireUser;
+window.newUser = newUser;
+window.UpdateSubsribeStatus = UpdateSubsribeStatus;
+window.getAllPlayers = getAllPlayers;
+window.initList = initList
+window.getUserRoster = getUserRoster
+
+window.secret_list_getter = secret_list_getter
+window.secret_list_adder = secret_list_adder
+window.secrect_list_dropper = secrect_list_dropper
+window.secret_list_sendBack = secret_list_sendBack
+window.secret_list_start = secret_list_start
+
+window.fireAuthOut = fireAuthOut
+window.getPlayerById = getPlayerById
+
+window.all_school = all_school
+window.conf_member = conf_member
