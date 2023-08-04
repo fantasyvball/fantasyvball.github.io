6b0154583e127bd74021630552de0a961b9111e0
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2023-08-04 18:52:13 -0400

    Update router_setup_.js

--- script/router_setup_.js
+++ script/router_setup_.js
@@ -1,18 +1,18 @@
-import NavbarComponent from './components/NavbarComponent.js';
+import NavbarComponent from './minified/NavbarComponent.js';
 import FooterComponent from './minified/FooterComponent.js';
 
 import LoginComponent from './minified/LoginComponent.js';
-import LogoutComponent from './components/LogoutComponent.js';
+import LogoutComponent from './minified/LogoutComponent.js';
 import HomeComponent from './minified/HomeComponent.js';
 import PlayerRosterComponent from './minified/PlayerRosterComponent.js'
-import UserComponent from './components/UserComponent.js'
+import UserComponent from './minified/UserComponent.js'
 
 import AvailablePlayersComponent from './minified/AvailablePlayersComponent.js'
-import ArticleComponent from './components/ArticleComponent.js'
-import LeaderBoardComponent from './components/LeaderBoardComponent.js'
-import PlayerAchievmentComponent from './components/PlayerAchievementComponent.js'
-import NotFoundComponent from './components/NotFoundComponent.js'
-import StatsComponent from './components/StatsComponent.js'
+import ArticleComponent from './minified/ArticleComponent.js'
+import LeaderBoardComponent from './minified/LeaderBoardComponent.js'
+import PlayerAchievmentComponent from './minified/PlayerAchievementComponent.js'
+import NotFoundComponent from './minified/NotFoundComponent.js'
+import StatsComponent from './minified/StatsComponent.js'
 
 const routes = [
   {path: "/", component: HomeComponent},
