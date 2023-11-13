4a9a663c74871c438e2d8d634caf09d460965a35
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2023-11-13 15:39:21 -0500

    Update RPIComponent.js

--- script/components/RPIComponent.js
+++ script/components/RPIComponent.js
@@ -122,7 +122,7 @@ export default{
       });
     })
 
-    const weeklabel = ["w1","w2","w3","w4","w5","w6","w7","w8","w9","w10","w11"]
+    const weeklabel = ["w1","w2","w3","w4","w5","w6","w7","w8","w9","w10","w11","w12"]
 
 
     
