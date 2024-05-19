bd161d32af35d9962cd5507c96827f89e3da22f1
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-05-19 16:51:16 -0700

    Update updateJson.py

--- test/updateJson.py
+++ test/updateJson.py
@@ -6,11 +6,8 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")  # Enable headless mode
 chrome_options.add_argument("--window-size=1920x1080")  # Set window size
 
-# Path to chromedriver executable (update this path to match your own)
-chromedriver_path = "/path/to/chromedriver"
-
 # Initialize the Chrome webdriver with the specified options
-driver = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options)
+driver = webdriver.Chrome(options=chrome_options)
 
 # Open Google
 driver.get("https://www.google.com")
